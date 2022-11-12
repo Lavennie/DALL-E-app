e6920c57e819705ee92e3cd65c6c755d5a6e0ebb
Lavennie <42303947+Lavennie@users.noreply.github.com>
2022-11-12 19:28:15 +0100

    Finished all 3 functionalities, config file, color theme
    Implemented functionalities: generate image (already before), variations and edit image.
    Added actual settings to config.txt file.
    Color theme.

--- dall-e.py
+++ dall-e.py
@@ -1,44 +1,150 @@
+# documentation: https://beta.openai.com/docs/guides/images/
 import openai
 import webbrowser
 import os
 
-BLACK_FG = "\033[30m"
+# at start so that colors work for error messages (light)
+os.system('color F0')
+
+FG_COLOR = "\033[30m" # black
+PROMPT_COLOR = "\033[34m" # blue
 RED_FG = "\033[31m"
 BLUE_FG = "\033[34m"
 GREEN_FG = "\033[32m"
 YELLOW_FG = "\033[93m"
 
-os.system('color F0')
-openai.api_key = ""
+def printError(msg):
+    print(RED_FG + msg + "\n")
+def imageGenerateCountPrompt():
+    return min(max(int(input(FG_COLOR + "Enter image count between 1-10" +
+        "(inputs will be limited to this range)\n" + PROMPT_COLOR)), 1), 10)
+    
 
+# read user settings
+configFilePath = "config.txt"
+colorTheme = "light"
+imageSize = "1024x1024"
+
+if (os.path.exists(configFilePath)):
+    for line in open(configFilePath):
+        stripped = line.strip()
+        # color theme
+        if (stripped.startswith("color-theme:")):
+            readTheme = (line.strip()[12:]).strip()
+            if(readTheme == "dark"):
+                colorTheme = "dark"
+        # generated image size
+        elif (stripped.startswith("size:")):
+            readSize = (line.strip()[5:]).strip()
+            if(readTheme in ["256x256", "512x512", "1024x1024"]):
+                imageSize = readSize
+        # key from openai account
+        elif (stripped.startswith("key:")):
+            readKey = (line.strip()[4:]).strip()
+            if(readKey.startswith("sk-") and len(readKey) == 51):
+                openai.api_key = readKey
+            else:
+                printError("Please add a \'key: [sk-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX]\' " +
+                           "line to \'config.txt\' file where \'sk-...\' represents a " +
+                           "key generated in your openai account")
+                input()
+                exit()
+                
+else:
+    printError("Please create a \'" + configFilePath + "\' file in same folder as this script.\n" +
+               "Avalible options inside config file:\n" +
+               "- key: [sk-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX]\n" +
+               "- color-theme: [light/dark]\n" +
+               "- size: [1024x1024/512x512/256x256]")
+    input()
+    exit()
+
+# dark color theme background
+if (colorTheme == "dark"):
+    os.system('color 07')
+    FG_COLOR = "\033[37m" # white
+    PROMPT_COLOR = "\033[36m" # yellow
+    
 inputText = ""
 generateCount = 0;
 mode = 0;
 
 while (inputText != "q"):
-    inputText = input(BLACK_FG + "Describe what you wish to generate or enter:\n- \'q\' to exit\n" +
-        "- \'c\' to generate image\n- \'e\' to edit existing image\n- \'v\' to generate variations\n" + BLUE_FG)
+    nextStepPrompt = ""
+    if (mode == 0):
+        nextStepPrompt = "Describe what you wish to generate"
+    elif (mode == 1):
+        nextStepPrompt = "Enter path to source image for image edit (absolute or relative to script folder)"
+    elif (mode == 2):
+        nextStepPrompt = "Enter path to source image for variations (absolute or relative to script folder)"
+        
+    inputText = input(FG_COLOR + nextStepPrompt + " or enter:\n- \'q\' to exit\n" +
+        "- \'c\' to generate image (default)\n- \'e\' to edit existing image\n- \'v\' to generate variations\n" + PROMPT_COLOR)
+
     if(inputText == "q"):
         break;
     elif(inputText == "c"):
-        mode = 0;
-        print(BLACK_FG + "Mode swapped to" + GREEN_FG + "\'Generate Image\'\n")
+        mode = 0
+        print(FG_COLOR + "Mode swapped to" + GREEN_FG + "\'Generate Image\'\n")
     elif(inputText == "e"):
-        print(RED_FG + "Not yet implemented, please choose another option\n")
+        mode = 1
+        print(FG_COLOR + "Mode swapped to" + GREEN_FG + "\'Edit Image\'\n")
     elif(inputText == "v"):
-        print(RED_FG + "Not yet implemented, please choose another option\n")
+        mode = 2
+        print(FG_COLOR + "Mode swapped to" + GREEN_FG + "\'Generate Image Variations\'\n")
     else:
-        generateCount = int(input(BLACK_FG + "Enter image count between 1-10 (inputs will be limited to this range)\n" + BLUE_FG))
-        if (mode == 0):
-            response = openai.Image.create(
-              prompt=inputText,
-              n=generateCount,
-              size="1024x1024"
-            )
-            
-            for image_url in response['data']:
-                webbrowser.open(image_url['url'], new=2)
-            print()
-        else:
-            print(RED_FG + "This mode is not yet imaplemented, please swap to another mode\n")
-        
+        try:
+            if (mode == 0):
+                generateCount = imageGenerateCountPrompt();
+                response = openai.Image.create(
+                  prompt = inputText,
+                  n = generateCount,
+                  size = imageSize
+                )
+                
+                for image_url in response['data']:
+                    webbrowser.open(image_url['url'], new=2)
+                print()
+            elif (mode == 1):
+                if (os.path.exists(inputText)):
+                    maskPath = input(FG_COLOR + "Enter path to mask image for image edit (absolute or relative to script folder)\n" + PROMPT_COLOR)
+                    if (os.path.exists(maskPath)):
+                        readPrompt = input(FG_COLOR + "Describe what you wish to generate\n" + PROMPT_COLOR)
+                        generateCount = imageGenerateCountPrompt();
+                        response = openai.Image.create_edit(
+                            image = open(inputText, "rb"),
+                            mask = open(maskPath, "rb"),
+                            prompt = readPrompt,
+                            n = generateCount,
+                            size = imageSize
+                        )
+                    
+                        for image_url in response['data']:
+                            webbrowser.open(image_url['url'], new=2)
+                        print()
+                    else:
+                        printError("Given path to mask image does not exist")
+                else:
+                    printError("Hiven path to source image does not exist")       
+            elif (mode == 2):
+                if (os.path.exists(inputText)):
+                    generateCount = imageGenerateCountPrompt();
+                    response = openai.Image.create_variation(
+                        image = open(inputText, "rb"),
+                        n = generateCount,
+                        size = imageSize
+                    )
+                    
+                    for image_url in response['data']:
+                        webbrowser.open(image_url['url'], new=2)
+                    print()
+                else:
+                    printError("Given path to source image does not exist")
+            else:
+                printError("This mode is not yet imaplemented, please swap to another mode")
+        except:
+            printError("Failed request. Possible reasons:\n" +
+                       "- Key in \'config.txt\' is invalid\n" +
+                       "- Limit of 10 images per minute or 25 images per 5 minutes has been reached, in this case please wait a bit")
+            input()
+            break
